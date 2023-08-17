3ea347d629ee2c078760801512a5ee6d8825f616
Bittu-co <nagpurebittu05@gmail.com>
2023-08-17 15:49:41 +0530

    java programme

---
+++ src/java/top.java
@@ -0,0 +1,11 @@
+public class top {
+    public static void main(String[] args) {
+int minutes=5;it
+        int hours =2;
+int seconds=minutes*60;
+int hourseconds= hours*60*60;
+
+System.out.println("hour to seconds: " + hourseconds);
+System.out.println("minutes to seconds :" + seconds);
+}
+}
